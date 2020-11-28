c137ba98ce1ab36bb6424e98f687683328002a81
huixiang.guo <huixiang.guo@vantopgroup.com>
2020-11-28 22:44:27 +0800

    初始化项目

---
+++ src/main/java/com/ghx/rocketmq/RocketmqProApplication.java
@@ -0,0 +1,13 @@
+package com.ghx.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghx/rocketmq/RocketmqProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghx.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
